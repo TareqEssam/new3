cbd7b8b55e175fa1d6582d0df7be56915413875b
TareqEssam <130806819+TareqEssam@users.noreply.github.com>
2026-03-08 19:22:01 +0200

    Update activity_database.js

--- js/activity_database.js
+++ js/activity_database.js
@@ -107,7 +107,7 @@ const masterActivityDB = [
         },
              {
             "name": "دليل اتحاد الصناعات المصرية ",
-            "link": "guides/دليل اتحاد الصناعات المصرية .pdf"
+            "link": "guides/dalil_etihad.pdf"
         },
              {
             "name": "دليل تقييم التأثير البيئى الخاص بالانشطه الاستثمارية",
@@ -8074,6 +8074,7 @@ const masterActivityDB = [
 
 // نشر قاعدة البيانات للمساعد
 window.masterActivityDB = masterActivityDB;
+
 
 
 
